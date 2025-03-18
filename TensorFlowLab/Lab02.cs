8de187a18ed040afb824e93e87bca4c589c6c809
Achira Raksakaew <achira@buzzebees.com>
2025-03-18 12:32:36 +0700

    commit fix issue add learning rate.

--- TensorFlowLab/Lab02.cs
+++ TensorFlowLab/Lab02.cs
@@ -1,5 +1,4 @@
-﻿using Tensorflow;
-using static Tensorflow.Binding;
+﻿using static Tensorflow.Binding;
 
 class Lab02
 {
@@ -7,6 +6,12 @@ class Lab02
     {
         var x_train = tf.constant(new float[] { 1, 2, 3, 4, 5 });
         var y_train = tf.constant(new float[] { 2, 4, 6, 8, 10 });
+
+        // กำหนดค่า Learning Rate
+        float learningRate = 0.01f;
+
+        // ใช้ SGD Optimizer จาก Keras
+        var optimizer = tf.keras.optimizers.SGD(learningRate);
 
         var W = tf.Variable(0.0f);
         var b = tf.Variable(0.0f);
@@ -20,23 +25,18 @@ class Lab02
             var prediction = W * x_train + b;
             var loss = tf.reduce_mean(tf.square(y_train - prediction));
 
-            var grads = tape.gradient(loss, new ResourceVariable[] { W, b });
+            var grads = tape.gradient(loss, [W, b]);
+            // ใช้ optimizer.apply_gradients()
+            optimizer.apply_gradients(zip(grads, [W, b]));
 
             W.assign_sub(0.01f * grads[0]);
             b.assign_sub(0.01f * grads[1]);
         }
 
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{Environment.NewLine}**********************");
         Console.WriteLine($"Trained W: {W.numpy()}");
         Console.WriteLine($"Trained b: {b.numpy()}");
-
+        Console.ResetColor();
     }
 }
-
-// กำหนดค่า Learning Rate
-//float learningRate = 0.01f;
-
-// สร้าง optimizer
-//var optimizer = tf.optimizers.SGD(learningRate);
-
-//อยู่ใต้ var grades
-//optimizer.apply_gradients(zip(grads, new Variable[] { W, b }));
--- TensorFlowLab/Program.cs
+++ TensorFlowLab/Program.cs
@@ -2,10 +2,10 @@
 {
     public static void Main()
     {
-        Lab01.Run();
+        Lab02.Run();
 
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("Press any key to exit...");
+        Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
         Console.ResetColor();
 
         Console.ReadKey();
